3d9b95b5b980fb35e64f42af02533bb209121874
d1ng0x <d1ng0x@mail.ru>
2020-12-08 19:15:06 +0300

    7 lessons

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -108,11 +108,8 @@ fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO()
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "z", "b" to "sweet")) -> true
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "zee", "b" to "sweet")) -> false
  */
-fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean {
-    if (b + a == b)
-        return true
-    return false
-}
+fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean = b + a == b
+
 
 /**
  * Простая (2 балла)
@@ -143,15 +140,7 @@ fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>): Map<Strin
  * В выходном списке не должно быть повторяюихся элементов,
  * т. е. whoAreInBoth(listOf("Марат", "Семён, "Марат"), listOf("Марат", "Марат")) == listOf("Марат")
  */
-fun whoAreInBoth(a: List<String>, b: List<String>): List<String> {
-    val res = mutableListOf<String>()
-    for (i in a.indices) {
-        val element = a[i]
-        if (element in b)
-            res += element
-    }
-    return res
-}
+fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = a.intersect(b).toList()
 
 /**
  * Средняя (3 балла)
@@ -183,22 +172,7 @@ fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<S
  *   averageStockPrice(listOf("MSFT" to 100.0, "MSFT" to 200.0, "NFLX" to 40.0))
  *     -> mapOf("MSFT" to 150.0, "NFLX" to 40.0)
  */
-fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> {
-    val res = mutableMapOf<String, Double>()
-    for ((key) in stockPrices) {
-        var i = 0
-        val x = key
-        var sum = 0.0
-        for ((key, value) in stockPrices)
-            if (key == x) {
-                sum += value
-                i++
-            }
-        res[key] = sum / i
-    }
-    return res
-}
-
+fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> = TODO()
 
 /**
  * Средняя (4 балла)
